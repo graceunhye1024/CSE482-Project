9391cdd1c54b95b439d603f64564febf7438d548
graceunhye1024 <parkeu24@msu.edu>
2023-12-04 02:14:42 -0500

    display user selection & recommendation

--- src/pages/PersonalizedRecommendations.js
+++ src/pages/PersonalizedRecommendations.js
@@ -3,6 +3,7 @@ import SearchBar from './components/SearchBar';
 import RatingMovie from './components/RatingMovie';
 import {query, getDocs, collection, where, doc, updateDoc} from "firebase/firestore";
 import {db} from "../firebaseConfig"
+import { useNavigate } from 'react-router-dom'
 
 const tempMovieList = [
     "2001: A Space Odyssey (1968)",
@@ -39,36 +40,62 @@ const tempMovieList = [
     "Wizard of Oz, The (1939)",
 ]
 
+const movieResults = [
+    "2001: A Space Odyssey (1968)",
+    "Die Hard (1988)",
+    "Dragonheart (1996)",
+    "E.T. the Extra-Terrestrial (1982)",
+    "Wizard of Oz, The (1939)",
+]
+
+const moviesRated = [
+    { title: "Seven (a.k.a. Se7en) (1995)", rating: 4 },
+    { title: "Silence of the Lambs, The (1991)", rating: 1 },
+    { title: "Rob Roy (1995)", rating: 5 },
+    { title: "Monty Python's Life of Brian (1979)", rating: 2 },
+];
+
 export default function PersonalizedRecommendations() {
+    const [displayCount, setDisplayCount] = useState(8); // State to track the number of movies displayed
     const [results, setResults] = useState([]);
     const [modalIsOpen, setModalIsOpen] = useState(false);
     const [currentTitle, setCurrentTitle] = useState("");
-    
+    const navigate = useNavigate();
+    const [ratedMovies, setRatedMovies] = useState([]);
+
+    const onMovieRated = (movieTitle, movieRating) => {
+        setRatedMovies(prevMovies => [...prevMovies, { title: movieTitle, rating: movieRating }]);
+    };
 
     const getUserRecommendations = async (movie) => {
-        //TODO: Get the current user's email
-        let queryForUser = query(collection(db, "users"), where("email", "==", "joelnataren9@hotmail.com")); 
-        let querySnapshot = await getDocs(queryForUser);
-        let userDoc = querySnapshot.docs[0];
+        // // TODO: Get the current user's email
+        // let queryForUser = query(collection(db, "users"), where("email", "==", "joelnataren9@hotmail.com")); 
+        // let querySnapshot = await getDocs(queryForUser);
+        // let userDoc = querySnapshot.docs[0];
 
-        let currentMoviesObj = userDoc.data().movies;
+        // let currentMoviesObj = userDoc.data().movies;
 
-        let data= {
-            "userMovies": currentMoviesObj,
-        }
-        console.log("data for user based:",data)
+        // let data= {
+        //     "userMovies": currentMoviesObj,
+        // }
+        // console.log("data for user based:",data);
 
-        let response = await fetch("http://localhost:8080/user-based-recommendation", {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data),
-        })
-        // TODO: Use the following object to display the recommendations
-        let recommendationsObj = await response.json();
-        console.log(recommendationsObj);
-        
+        // let response = await fetch("http://localhost:8080/user-based-recommendation", {
+        //     method: "POST",
+        //     headers: {
+        //         'Content-Type': 'application/json'
+        //     },
+        //     body: JSON.stringify(data),
+        // })
+        // // TODO: Use the following object to display the recommendations
+        // let recommendationsObj = await response.json();
+        // console.log(recommendationsObj);
+        displayMovieRecommendations(movieResults);  // uncomment the above lines and delete this line
+                                                    // this is displaying temporary results as backend is not running
+    }
+    
+    const displayMovieRecommendations = (movies) => {
+        navigate("/movie-recommendations-result", { state: { recommendation: "user", movies: movies } });
     }
 
     const toggleModal = (title) => {
@@ -79,21 +106,45 @@ export default function PersonalizedRecommendations() {
     return (
         <section className='main-content'> 
             <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> 
-            <div className="absolute w-[80vw] bg-black text-white flex flex-col z-30 p-3 h-[80vh] justify-center items-center"> 
+            <div className="absolute w-[80vw] bg-black text-white flex flex-col z-30 p-3 h-[80vh] justify-center items-center">
                 <h1 className='text-2xl'>Personalized Picks</h1>
                 <p className='text-lg'>Rate the following movies to get personalized recommendations</p>
-                <SearchBar onSearch={setResults} />
-                {results.map((title, index) => (
-                    <button key={index} onClick={() => toggleModal(title)} className='p-2 bg-green'>
-                        {title}
-                    </button>
-                ))}
-                <button onClick={getUserRecommendations} className='p-2 bg-green'>
+                <div className="content-container" style={{ display: 'flex', justifyContent: 'space-between', maxHeight: '50%', margin:'5px' }}>
+                    <div className='search-section' style={{ flex: 1, padding: '10px' }}>
+                        <SearchBar onSearch={setResults} />
+                        {results.map((title, index) => (
+                            <button key={index} onClick={() => toggleModal(title)} className='p-2 bg-green'>
+                                {title}
+                            </button>
+                        ))}
+                    </div>
+                    <div className='movie-ratings'>
+                        {/* moviesRated is temporary variable (because backend could not be run)
+                            replace moviesRated to ratedMovies, which are retrieved from RatingMovie.js */}
+                        {moviesRated.length > 0 && (
+                            <div className="rated-movies" style={{ flex: 1, maxHeight: '100%', width: '300px', padding: '10px', overflow: 'auto' }}>
+                                {moviesRated.map((movie, index) => (
+                                    <div key={index} style={{ margin: '5px', border: '2px solid grey', wordWrap: 'break-word', padding: '5px'}}>
+                                        <p>{movie.title} : <b>{movie.rating}</b></p>
+                                    </div>
+                                ))}
+                            </div>
+                        )}
+                    </div>
+                </div>
+                <button onClick={getUserRecommendations} className='rounded-md  p-2 bg-green-500 text-gray-700 m-5'>
                     Get Recommendations
+                </button>
+                <button className='absolute bottom-3 right-3 p-2' onClick={() => navigate("/")}>
+                    Back Home
                 </button>
             </div> 
             {
-                modalIsOpen && <RatingMovie modalIsOpen={modalIsOpen} setModalIsOpen={setModalIsOpen} currentTitle={currentTitle} />
+                modalIsOpen && <RatingMovie 
+                                  modalIsOpen={modalIsOpen} 
+                                  setModalIsOpen={setModalIsOpen} 
+                                  currentTitle={currentTitle} 
+                                  onMovieRated={onMovieRated} />
             }
         </section>
   )
--- src/pages/SimilarMovies.js
+++ src/pages/SimilarMovies.js
@@ -1,5 +1,22 @@
 import React, {useEffect, useState} from 'react'
 import SearchBar from './components/SearchBar';
+import { useNavigate } from 'react-router-dom'
+
+const movieResults = [
+    "Silence of the Lambs, The (1991)",
+    "Star Wars: Episode IV - A New Hope (1977)",
+    "Star Wars: Episode V - The Empire Strikes Back (1980)",
+    "Terminator 2: Judgment Day (1991)",
+    "Twelve Monkeys (a.k.a. 12 Monkeys) (1995)",
+    "Usual Suspects, The (1995)",
+    "What's Eating Gilbert Grape (1993)",
+    "Léon: The Professional (a.k.a. The Professional) (Léon) (1994)",
+    "Mask, The (1994)",
+    "Monty Python and the Holy Grail (1975)",
+    "Monty Python's Life of Brian (1979)",
+    "One Flew Over the Cuckoo's Nest (1975)",
+    "Platoon (1986)",
+]
 
 /**
  * Uses the search bar component to get a list of movies
@@ -10,33 +27,47 @@ import SearchBar from './components/SearchBar';
 export default function SimilarMovies() {
     const [results, setResults] = useState([]);
     const [recommendations, setRecommendations] = useState([]);
-    const [movies, setMoviesList] = useState([]);
+    const [movies, setMoviesList] = useState([
+                                                "Inception (2010)",
+                                                "The Matrix (1999)",
+                                                "Interstellar (2014)",
+                                                "Raiders of the Lost Ark (Indiana Jones and the Raiders of the Lost Ark) (1981)",
+                                                "Silence of the Lambs, The (1991)",
+                                            ]);  // clear out the list inside later when connected to backend
+    const navigate = useNavigate();
 
-    console.log(results)
+    console.log(results);
 
     /**
      * 
      * @param movies: list of movies to get recommendations for
      */
     const getMovieBasedRecommendations = async (movies) => {
-        try {
-            console.log(movies)
-            let listofMovies = movies;
-            let obj = {
-                "movies": listofMovies,
-            }
-            let response = await fetch("http://localhost:8080/movie-based-recommendation", {
-                method: "POST",
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(obj),
-            })
-            let json = await response.json();
-            console.log(json);
-        } catch (error) {
-            console.log(error)
-        }
+        // try {
+        //     console.log(movies)
+        //     let listofMovies = movies;
+        //     let obj = {
+        //         "movies": listofMovies,
+        //     }
+        //     let response = await fetch("http://localhost:8080/movie-based-recommendation", {
+        //         method: "POST",
+        //         headers: {
+        //             'Content-Type': 'application/json'
+        //         },
+        //         body: JSON.stringify(obj),
+        //     })
+        //     let json = await response.json();
+        //     console.log(json);
+        //     displayMovieRecommendations(json);
+        // } catch (error) {
+        //     console.log(error)
+        // }
+        displayMovieRecommendations(movieResults);  // uncomment the above lines and delete this line
+                                                    // this is displaying temporary results as backend is not running
+    }
+    
+    const displayMovieRecommendations = (movies) => {
+        navigate("/movie-recommendations-result", { state: { recommendation: "movie", movies: movies } });
     }
 
     return (
@@ -45,23 +76,39 @@ export default function SimilarMovies() {
             <div className="absolute w-[80vw] bg-black text-white flex flex-col gap-3 z-30 p-3 h-[80vh] justify-center items-center"> 
                 <h1 className='text-2xl'>Find similar movies</h1>
                 <p className='text-lg'>Enter a movie title to find similar movies</p>
-                <SearchBar onSearch={setResults} />
-                <div className='flex flex-col gap-3 justify-center items-center text-black'>
-                    {results.map((title, index) => (
-                        <div key={index} className='flex flex-row gap-3 p-2 bg-gray-100 rounded-md'>
-                            <button  className=''>
-                                {title}
-                            </button>
-                            <button  onClick={() => {setMoviesList([...movies, title])}} className='p-2 bg-green-500 text-gray-700'>
-                                Add
-                            </button>
+                <div className="content-container" style={{ display: 'flex', justifyContent: 'space-between', maxHeight: '50%', margin:'5px' }}>
+                    <div className='search-section'  style={{ flex: 1, padding: '10px' }}>
+                        <SearchBar onSearch={setResults}/>
+                        <div className='flex flex-col gap-3 justify-center items-center text-black'>
+                            {results.map((title, index) => (
+                                <div key={index} className='flex flex-row gap-3 p-2 bg-gray-100 rounded-md'>
+                                    <button  className=''>
+                                        {title}
+                                    </button>
+                                    <button  onClick={() => {setMoviesList([...movies, title])}} className='p-2 bg-green-500 text-gray-700'>
+                                        Add
+                                    </button>
+                                </div>
+                            ))}
                         </div>
-                    ))}
+                    </div>
+                    <div className='added-movies'>
+                        {movies.length > 0 && (
+                            <div className="added-movies" style={{ flex: 1, maxHeight: '100%', width: '300px', padding: '10px', overflow: 'auto' }}>
+                                <h2 style={{ margin: '5px'}}>Movies Added</h2>
+                                {movies.map((movie, index) => (
+                                    <p key={index} style={{ margin: '5px', border: '2px solid grey', wordWrap: 'break-word', padding: '5px'}}>{movie}</p>
+                                ))}
+                            </div>
+                        )}
+                    </div>
                 </div>
-                <button onClick={() => {getMovieBasedRecommendations(movies)}} className='rounded-md  p-2 bg-green-500 text-gray-700'>
+                <button onClick={() => {getMovieBasedRecommendations(movies)}} className='rounded-md  p-2 bg-green-500 text-gray-700 m-5'>
                     Get Recommendations
                 </button>
-                {/* Show recommendations when they are fetched */}
+                <button className='absolute bottom-3 right-3 p-2' onClick={() => navigate("/")}>
+                    Back Home
+                </button>
             </div> 
         </section>
   )
